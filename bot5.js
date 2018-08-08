783b845cfc489b054538969262e586d4dd1cb710
shkshk2 <42033717+shkshk2@users.noreply.github.com>
2018-08-08 21:44:05 +0200

    Create bot5.js

---
+++ bot5.js
@@ -0,0 +1,69 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'باك') {
+    msg.reply('ولكم!');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('عليكم السلام!');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكو') {
+    msg.reply('عليكو السلام!');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'هلا') {
+    msg.reply('هلا بيك!');
+  }
+
+  
+
+client.on('message', msg => {
+  if (msg.content === '!') {
+    msg.reply('Welcome To CN CLan :hibiscus::beers: !!');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('تيت!');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'هاي') {
+    msg.reply('هايات!');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'باي') {
+    msg.reply('لا تتاخر!');
+  }
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN5);.
