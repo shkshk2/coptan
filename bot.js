3730f4efeb0ed40c27e90cbc93b401fb29a8c7fb
shkshk2 <42033717+shkshk2@users.noreply.github.com>
2018-08-09 02:28:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,80 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const prefix = '!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
 client.on('message', msg => {
-  if(msg.content === 'باك')
-  msg.reply('ولكم')
+  if (msg.content === 'باك') {
+    msg.reply('ولكم!');
+  }
 });
 
 
 client.on('message', msg => {
-  if(msg.content === 'السلام عليكم')
-  msg.reply('عليكم السلام')
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('عليكم السلام!');
+  }
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكو') {
+    msg.reply('عليكو السلام!');
+  }
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('تيت!');
+  }
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'هاي') {
+    msg.reply('هايات!');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'باي') {
+    msg.reply('لا تتاخر!');
+  }
 });
 
 
 client.on('message', msg => {
-  if(msg.content === 'السلام عليكو')
-  msg.reply('عليكو السلام')
+  if (msg.content === 'نعم') {
+    msg.reply('انعم الله عليك!');
+  }
 });
+
+
 
 
 client.on('message', msg => {
-  if(msg.content === 'برب')
-  msg.reply('تيت')
+  if (msg.content === '!') {
+    msg.reply('ولكم منور يا عسل!');
+  }
 });
 
 
-client.on('message', msg => {
-  if(msg.content === 'هاي')
-  msg.reply('هايات')
-});
 
 
-client.on('message', msg => {
-  if(msg.content === 'باي')
-  msg.reply('لا تتاخر')
-});
 
 
-client.on('message', msg => {
-  if(msg.content === 'نعم')
-  msg.reply('انعم الله عليك')
-});
 
 
 client.login(process.env.BOT_TOKEN);
