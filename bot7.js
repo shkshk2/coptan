f833b35f1ffd2de00e8f16d6e3fcc0ba5290e5cd
shkshk2 <42033717+shkshk2@users.noreply.github.com>
2018-08-09 14:22:40 +0200

    Create bot7.js

---
+++ bot7.js
@@ -0,0 +1,24 @@
+client.on('guildMemberRemove', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`خرج عضو`)
+    .setDescription(`الى اللقاء...`)
+    .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RED')
+    .setFooter(`The King Bot`, '')
+
+var channel =member.guild.channels.find('name', 'wlc')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN7);
