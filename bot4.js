bb98d526185d4498e63e6600f9a9ca2b7fe9a83d
shkshk2 <42033717+shkshk2@users.noreply.github.com>
2018-08-11 05:44:24 +0200

    Create bot4.js

---
+++ bot4.js
@@ -0,0 +1,79 @@
+client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === "$help") {
+  let embed = new Discord.RichEmbed()
+          .setAuthor(message.author.username, message.author.avatarURL)
+           .setThumbnail(message.author.avatarURL)
+                 .setTimestamp()
+    .setDescription(`
+***
+:white_small_square:  ( اوامر الادارة***
+**
+:small_orange_diamond:   البوت يكتب الي ��نت تكتبه في صورة
+$say
+:small_orange_diamond:   لمسح الشات
+$clear
+:small_orange_diamond:   للباند
+$ban
+:small_orange_diamond:   للطرد
+$kick
+:small_orange_diamond:   للارسال لاعضاء السيرفر بشكل مطور
+$bc
+:small_orange_diamond:   للارسال لاعضاء السيرفر بشكل عادي
+$bcr
+:small_orange_diamond:   لأعطاء شخص ميوت بالسيرفر - يلزم ان يكون بالسيرفر رتبة Muted
+$mute
+:small_orange_diamond:   لفك الميوت عن شخص
+$unmute
+:small_orange_diamond:   لااعطاء شخص رتبه
+$role humans [role]
+:small_orange_diamond:   لااعطاء الجميع رتبه
+$role all [role]
+:small_orange_diamond:   لااعطاء البوتات رتبه
+$role bots [role]
+----
+**
+***
+:white_small_square: ( الاوامر العامة***
+**
+:small_blue_diamond:  للابلغ عن شخص
+$report
+:small_blue_diamond:  لعرض التاريخ والوقت 
+$day
+:small_blue_diamond:  للارسال اقتراح للادراه
+$suggest
+:small_blue_diamond:  معلومات السيرفر
+$server
+:small_blue_diamond:  لعرض اسكنك في ماين كرافت بشكل مطور
+$mc3d
+:small_blue_diamond:   اسكنك في ماين كرافت
+$mcskin
+:small_blue_diamond:   راس اسكنك في ماين كرافت
+$skin
+:small_blue_diamond:   اسئله عن ماين كرافت
+$minecraft
+:small_blue_diamond:   لااستعراض الحاسبه
+$calculate
+:small_blue_diamond:   احصائيات فورت نايت
+$fortnite
+ ----
+رابط سيرفر البوت
+ 
+----------
+رابط دعوة البوت
+
+
+**
+
+`)
+.setColor('RANDOM')
+message.author.sendEmbed(embed)
+}
+});
+
+client.on('message', msg => {
+      if(!msg.channel.guild) return;
+    if(msg.content.startsWith (prefix  + 'help')) {
+    msg.reply('**📩 شيك علي الخاص**');
+  }
+});
