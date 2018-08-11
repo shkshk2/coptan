1e2be48c27029c98bb45e006612b68187d4ca3c7
shkshk2 <42033717+shkshk2@users.noreply.github.com>
2018-08-11 05:01:48 +0200

    Update bot9.js

--- bot9.js
+++ bot9.js
@@ -1,7 +1,8 @@
-client.on('message', message => {
+client.on('message',
+message =>{
     if (message.author.bot) return;
-     if (message.content === prefix + "help-en") {
-		 message.channel.send('**The Message Was Sent On Private**');
+     if (message.content === prefix + "help-en")    {
+        message.channel.send('**The Message Was Sent On Private**');
             
 	
 		 
@@ -9,89 +10,72 @@ client.on('message', message => {
 
  message.author.sendMessage(`
  **
-__~~CN 4 EveR bot~~__ By: Peery#7863
+__~~CN 4 EveR bot~~__ By:Peery#7863
 ╭━━━┳━╮╱╭╮╭╮╱╭╮╱╱╭╮╭━━━╮
 ┃╭━╮┃┃╰╮┃┃┃┃╱┃┃╱╱┃┃┃╭━╮┃
 ┃┃╱╰┫╭╮╰╯┃┃╰━╯┣━━┫┃┃╰━╯┃
 ┃┃╱╭┫┃╰╮┃┃┃╭━╮┃┃━┫┃┃╭━━╯
 ┃╰━╯┃┃╱┃┃┃┃┃╱┃┃┃━┫╰┫┃
 ╰━━━┻╯╱╰━╯╰╯╱╰┻━━┻━┻╯
-╔[❖════════════❖]╗
+╔        [
+            ❖════════════❖
+        ]        ╗
              Prefix = ' * '
-╚[❖════════════❖]╝
-
-╔[❖════════════❖]╗
+╚        [
+            ❖════════════❖
+        ]        ╝
+╔        [
+            ❖════════════❖
+        ]        ╗
              Admin Commands
-╚[❖════════════❖]╝
-
+╚        [
+            ❖════════════❖
+        ]        ╝
  ❖ *kick <mention > ➾ kick member from server
-
  ❖ *clear ➾ clear chat ( not ready yet fixing)
  
  ❖ *mute < mention > ➾ mute member
-
  ❖ *unmute <mention> ➾ unmute member
-
  ❖ *ct <name> ➾ create channel
-
  ❖ *cv <name> create voice channel
   
  ❖ *bc <message> ➾ message all members in server
-
-
-╔[❖════════════❖]╗
+╔        [
+            ❖════════════❖
+        ]        ╗
             General  Commands
-╚[❖════════════❖]╝
-
+╚        [
+            ❖════════════❖
+        ]        ╝
 ❖ *roll <number> ➾ role 
-
 ❖ *date ➾ see date
-
 ❖ *member ➾ members info
-
 ❖ *ser-av ➾ server avatar
-
 ❖ *uptime ➾ to see uptime
-
 ❖ *invs ➾ server invite link
-
 ❖ *own ➾ bot owner
-
 ❖ *id ➾ your id
-
 ❖ *avatar ➾ your avatar account
-
 ❖ *help-en ➾ help by arabic
-
 ❖ *help-en ➾ help by english
-
 ❖ *ping ➾ to see ping
-
 ❖ *bot ➾ bot informations 
-
 ❖ *server ➾ server informations 
-
 ❖ *invite ➾ bot invite link
-
-╔[❖════════════❖]╗
+╔        [
+            ❖════════════❖
+        ]        ╗
                     Welcome
-╚[❖════════════❖]╝
-
+╚        [
+            ❖════════════❖
+        ]        ╝
 to enable welcome message do channel name "wlc"
-
-
 ==================================================================
-
-Server support: https://discord.gg/uXvYbtB
-
+Server support:        https:        //discord.gg/uXvYbtB
 ==================================================================
-
-bot invite link: //discordapp.com/api/oauth2/authorize?client_id=477059195720433685&permissions=8&scope=bot
-
+bot invite link://discordapp.com/api/oauth2/authorize?client_id=477059195720433685&permissions=8&scope=bot
 ==================================================================
-
 `);
-
     }
 });
 
@@ -99,3 +83,4 @@ bot invite link: //discordapp.com/api/oauth2/authorize?client_id=477059195720433
 
 
 client.login(process.env.BOT_TOKEN9);
+package.json
